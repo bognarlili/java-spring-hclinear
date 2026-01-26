e0198fdecdaa6e42c18b23ac0cbe288a3ac4910b
bognarlili02 <bognar.lili02@gmail.com>
2026-01-26 22:08:17 +0100

    Optimize team listing with fetch join for players and captain

--- football-transfer/src/main/java/com/hcLinear/backendTest/repository/TeamRepository.java
+++ football-transfer/src/main/java/com/hcLinear/backendTest/repository/TeamRepository.java
@@ -1,9 +1,31 @@
 package com.hcLinear.backendTest.repository;
 
+import com.hcLinear.backendTest.dto.team.TeamListResponse;
+import com.hcLinear.backendTest.dto.team.TeamResponse;
+import com.hcLinear.backendTest.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hcLinear.backendTest.model.Team;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+
+    boolean existsByName(String name);
+
+    @Query("""
+      select distinct t
+      from Team t
+      left join fetch t.player
+      left join fetch t.captain
+    """)
+    List<Team> findAllWithPlayersAndCaptain();
+
+
+
+
+
 
 }
