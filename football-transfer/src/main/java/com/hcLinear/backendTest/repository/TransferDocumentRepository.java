4b2e717d495d154af5220a7541f431db8be62889
bognarlili02 <bognar.lili02@gmail.com>
2026-01-27 23:31:45 +0100

    add Transfer Document Repository

---
+++ football-transfer/src/main/java/com/hcLinear/backendTest/repository/TransferDocumentRepository.java
@@ -0,0 +1,7 @@
+package com.hcLinear.backendTest.repository;
+
+import com.hcLinear.backendTest.model.TransferDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferDocumentRepository extends JpaRepository<TransferDocument, Long>{
+}
