ee1c9fa8c50376dc8e80a00ac53bcf7ce177c733
bognarlili02 <bognar.lili02@gmail.com>
2026-01-27 20:39:00 +0100

    add reject transfer request operation

--- football-transfer/src/main/java/com/hcLinear/backendTest/repository/TransferRequestRepository.java
+++ football-transfer/src/main/java/com/hcLinear/backendTest/repository/TransferRequestRepository.java
@@ -1,7 +1,13 @@
 package com.hcLinear.backendTest.repository;
 
 import com.hcLinear.backendTest.model.TransferRequest;
+import com.hcLinear.backendTest.model.TransferRequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransferRequestRepository extends JpaRepository<TransferRequest, Long> {
+
+    List<TransferRequest> findByPlayerIdAndStatus(Long playerId, TransferRequestStatus status);
+
 }
