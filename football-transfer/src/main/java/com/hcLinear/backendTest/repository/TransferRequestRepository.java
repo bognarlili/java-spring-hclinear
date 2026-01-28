a50aaf019172de1e0ee81f5d19ccd137026a4ee2
bognarlili02 <bognar.lili02@gmail.com>
2026-01-28 01:08:46 +0100

    extend TransferRequestRepository with JpaSpecificationExecutor

--- football-transfer/src/main/java/com/hcLinear/backendTest/repository/TransferRequestRepository.java
+++ football-transfer/src/main/java/com/hcLinear/backendTest/repository/TransferRequestRepository.java
@@ -3,10 +3,11 @@ package com.hcLinear.backendTest.repository;
 import com.hcLinear.backendTest.model.TransferRequest;
 import com.hcLinear.backendTest.model.TransferRequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface TransferRequestRepository extends JpaRepository<TransferRequest, Long> {
+public interface TransferRequestRepository extends JpaRepository<TransferRequest, Long>, JpaSpecificationExecutor<TransferRequest> {
 
     List<TransferRequest> findByPlayerIdAndStatus(Long playerId, TransferRequestStatus status);
 
