2d5f1878f708364e8a7b27476ccd31ade1381922
bognarlili02 <bognar.lili02@gmail.com>
2026-01-28 01:11:46 +0100

    add specification for filtering transfer requests

---
+++ football-transfer/src/main/java/com/hcLinear/backendTest/service/TransferRequestSpecification.java
@@ -0,0 +1,28 @@
+package com.hcLinear.backendTest.service;
+
+import com.hcLinear.backendTest.model.*;
+import org.springframework.data.jpa.domain.Specification;
+
+public class TransferRequestSpecification {
+
+    public static Specification<TransferRequest> hasPlayerId(Long playerId) {
+        return (root, cq, cb) ->
+                cb.equal(root.get(TransferRequest_.player).get(Player_.id), playerId);
+    }
+
+    public static Specification<TransferRequest> hasFromTeamId(Long fromTeamId) {
+        return (root, cq, cb) ->
+                cb.equal(root.get(TransferRequest_.fromTeam).get(Team_.id), fromTeamId);
+    }
+
+    public static Specification<TransferRequest> hasToTeamId(Long toTeamId) {
+        return (root, cq, cb) ->
+                cb.equal(root.get(TransferRequest_.toTeam).get(Team_.id), toTeamId);
+    }
+
+    public static Specification<TransferRequest> hasStatus(TransferRequestStatus status) {
+        return (root, cq, cb) ->
+                cb.equal(root.get(TransferRequest_.status), status);
+    }
+
+}
